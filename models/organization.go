8e37f48902c7597be8a2835a6114ef90443fb653
monicapharm <maliuqing123@gmail.com>
2019-04-07 20:51:28 -0700

    bug fix

--- models/organization.go
+++ models/organization.go
@@ -3,21 +3,21 @@ package models
 // Organization represents information for a new org
 type Organization struct {
 	OrgId         int      `bson:"_id"`
-	OrgTitle      string   `json:"OrgTitle"`
-	OrgWebsite    string   `json:"OrgWebsite"`
-	StreetAddress string   `json: "StreetAddress"`
-	City          string   `json: "City"`
-	State         string   `json: "state"`
-	ZipCode       string   `json: "ZipCode"`
-	Phone         string   `json: "Phon"`
-	Email         string   `json: "Email"`
-	ActivityDesc  string   `json: "ActivityDesc"`
-	Lat           float64  `json: "Lat"`
-	Long          float64  `json: "Long"`
-	HasShadow     bool     `json: "HasShadow"`
-	HasCost       bool     `json: "HasCost"`
-	HasTransport  bool     `json: "HasTransport"`
-	Under18       bool     `json: "Under18"`
-	CareerEmp     []string `json: "CareerEmp"`
-	GradeLevels   []int    `json: "GradeLevels"`
+	OrgTitle      string   `bson:"OrgTitle"`
+	OrgWebsite    string   `bson:"OrgWebsite"`
+	StreetAddress string   `bson:"StreetAddress"`
+	City          string   `bson:"City"`
+	State         string   `bson:"State"`
+	ZipCode       string   `bson:"ZipCode"`
+	Phone         string   `bson:"Phone"`
+	Email         string   `bson:"Email"`
+	ActivityDesc  string   `bson:"ActivityDesc"`
+	Lat           float64  `bson:"Lat"`
+	Long          float64  `bson:"Long"`
+	HasShadow     bool     `bson:"HasShadow"`
+	HasCost       bool     `bson:"HasCost"`
+	HasTransport  bool     `bson:"HasTransport"`
+	Under18       bool     `bson:"Under18"`
+	CareerEmp     []string `bson:"CareerEmp"`
+	GradeLevels   []int    `bson:"GradeLevels"`
 }
