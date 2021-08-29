77eb8231f45532d091a7fa9fadfa753683a90975
Loi Cheng <loi@loico.us>
2021-08-29 17:18:55 -0400

    docker

--- build-local.sh
+++ build-local.sh
@@ -4,7 +4,7 @@ GOOS=linux go build
 cd ..
 
 # make docker
-docker build -f docker_nwhcp-gateway.dockerfile -t loibucket/nwhcp-gateway . # update to your username/nwhcp-gateway
+docker build -t loibucket/nwhcp-gateway . # update to your username/nwhcp-gateway
 docker rm -f gateway || true # mostly for testing because have to remove docker img every time you redeploy
 
 # Run server from docker img
