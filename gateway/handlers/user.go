5df6ea6d77de6027bd757645ab70dafdbf2e6fd4
Tyrah <tyrah96@gmail.com>
2023-03-02 18:16:52 -0800

    refactor favorite and unfavorite routes to update orgs and users

--- gateway/handlers/user.go
+++ gateway/handlers/user.go
@@ -298,21 +298,36 @@ func (uc UserController) AddOrgToFavorite(w http.ResponseWriter, r *http.Request
 	}
 
 	// grab user
-	selector := bson.M{
+	u_selector := bson.M{
 		"_id": bson.M{
 			"$eq": oid,
 		},
 	}
 
 	// add orgId to favorites
-	change := bson.M{
+	u_change := bson.M{
 		"$addToSet": bson.M{
 			"favoritedOrganizations": ooid,
 		},
 	}
 
-	// update db
-	uc.session.Database("mongodb").Collection("usersTest").UpdateOne(context.TODO(), selector, change)
+	// update user doc
+	uc.session.Database("mongodb").Collection("usersTest").UpdateOne(context.TODO(), u_selector, u_change)
+
+	// grab org
+	o_selector := bson.M{
+		"_id": bson.M{
+			"$eq": ooid,
+		},
+	}
+
+	o_change := bson.M{
+		"$addToSet": bson.M{
+			"favorited": oid,
+		},
+	}
+	// update org doc
+	uc.session.Database("mongodb").Collection("organizations").UpdateOne(context.TODO(), o_selector, o_change)
 
 }
 
@@ -348,95 +363,23 @@ func (uc UserController) DeleteOrgFavorite(w http.ResponseWriter, r *http.Reques
 		},
 	}
 
+	// grab org
+	o_selector := bson.M{
+		"_id": bson.M{
+			"$eq": ooid,
+		},
+	}
+
+	o_change := bson.M{
+		"$pull": bson.M{
+			"favorited": oid,
+		},
+	}
+
 	// delete orgId from user's AllOrgs.FavoritedOrg
 	uc.session.Database("mongodb").Collection("usersTest").UpdateOne(context.TODO(), selector, change)
 
+	// delete userId from AllUsers.Favorited
+	uc.session.Database("mongodb").Collection("organizations").UpdateOne(context.TODO(), o_selector, o_change)
+
 }
-
-// UserOrgs represents a program administrator's organizations
-// needed??
-// type UserOrgs struct {
-// 	ID        int64                `json:"id"`
-// 	Email     string               `json:"-"` //never JSON encoded/decoded
-// 	FirstName string               `json:"firstName"`
-// 	LastName  string               `json:"lastName"`
-// 	Orgs      []*orgs.Organization `json:"orgs"`
-// }
-
-// Validate validates the new user and returns an error if
-// any of the validation rules fail, or nil if its valid
-// func (nu *NewUser) Validate() error {
-// 	_, err := mail.ParseAddress(nu.Email)
-// 	if err != nil {
-// 		return fmt.Errorf("Invalid Email")
-// 	}
-// 	if len(nu.Password) < 6 {
-// 		return fmt.Errorf("Password must be at least 6 characters")
-// 	}
-// 	if len(nu.Password) != len(nu.PasswordConf) || strings.Compare(nu.Password, nu.PasswordConf) != 0 {
-// 		return fmt.Errorf("Password and confirmation do not match")
-// 	}
-// 	return nil
-// }
-
-// ToUser converts the NewUser to a User, setting the
-// PassHash field appropriately
-// func (nu *NewUser) ToUser() (*User, error) {
-// 	err := nu.Validate()
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	user := &User{}
-// 	user.FirstName = nu.FirstName
-// 	user.LastName = nu.LastName
-// 	user.Email = nu.Email
-// 	joinDate := time.Now().Format("01-02-2006")
-// 	user.JoinDate = joinDate
-// 	user.SetPassword(nu.Password)
-
-// 	return user, nil
-// }
-
-// FullName returns the user's full name, in the form:
-// "<FirstName> <LastName>"
-// If either first or last name is an empty string, no
-// space is put between the names. If both are missing,
-// this returns an empty string
-// func (u *User) FullName() string {
-// 	if len(u.FirstName) == 0 && len(u.LastName) == 0 {
-// 		return ""
-// 	} else if len(u.FirstName) == 0 {
-// 		return u.LastName
-// 	} else if len(u.LastName) == 0 {
-// 		return u.FirstName
-// 	} else {
-// 		return u.FirstName + " " + u.LastName
-// 	}
-// }
-
-// SetPassword hashes the password and stores it in the PassHash field
-// func (u *User) SetPassword(password string) error {
-// 	temp, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	u.PassHash = temp
-// 	return nil
-// }
-
-// Authenticate compares the plaintext password against the stored hash
-// and returns an error if they don't match, or nil if they do
-// func (u *User) Authenticate(password string) error {
-// 	return bcrypt.CompareHashAndPassword(u.PassHash, []byte(password))
-// }
-
-// // ApplyUpdates applies the updates to the user. An error
-// // is returned if the updates are invalid
-// func (u *User) ApplyUpdates(updates *Updates) error {
-// 	if updates.FirstName == "" && updates.LastName == "" {
-// 		return fmt.Errorf("names cannot both be null")
-// 	}
-// 	u.FirstName = updates.FirstName
-// 	u.LastName = updates.LastName
-// 	return nil
-// }
